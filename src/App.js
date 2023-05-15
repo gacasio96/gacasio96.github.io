a09b4cf911a872828cdb6340cb1ed407e090dc02
gacasio96 <geordetteacasio2018@gmail.com>
2023-05-15 16:13:02 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My online portfolio is coming soon.
         </p>
         <a
           className="App-link"

90c17a4e17e96e369ede778a0bacc6d45aabad95
gacasio96 <geordetteacasio2018@gmail.com>
2023-05-16 15:25:20 +0800

    Started working on the landing page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { Divide as Hamburger } from 'hamburger-react';
+import { VscGithub } from 'react-icons/vsc';
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My online portfolio is coming soon.
-        </p>
+        <Hamburger rounded />
       </header>
+
+      <section className="App-hero">
+        <div className="content">
+          <h1>Hello there!</h1><br/>
+          <h3>I'm G, a front-end web developer / designer based in the Philippines.</h3>
+          <div className="socials">
+            <div className="social-icons">
+              <a href="https://github.com/gacasio96" target="_blank"><VscGithub /></a>
+            </div>            
+            <button href="#">Download resume</button>
+          </div>
+        </div>
+        <div className="image">
+          <img src="/G - Avatar.png" className="avatar" />
+        </div>   
+      </section>
+
     </div>
   );
 }

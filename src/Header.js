e1190a9341ada5cdd49ec5fc5a81efd3791c7c76
gacasio96 <geordetteacasio2018@gmail.com>
2023-05-21 20:15:09 +0800

    minor edits

--- src/Header.js
+++ src/Header.js
@@ -3,20 +3,21 @@ import { Divide as Hamburger } from 'hamburger-react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 function Header() {
   return (
   <header>
-    <Container fluid>
+    <Container>
       <Row>
         <Col>
             <Hamburger rounded />
-  
         </Col>
       </Row>
     </Container>
   </header>
-
   );
 }
 

a98595c3f52db43a6b159fb71e293d08cc621e8d
gacasio96 <geordetteacasio2018@gmail.com>
2023-05-29 22:28:47 +0800

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -3,17 +3,24 @@ import { Divide as Hamburger } from 'hamburger-react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 
+import { useState } from 'react';
+
 function Header() {
+  const [isOpen, setOpen] = useState(false);
   return (
   <header>
     <Container>
       <Row>
-        <Col>
-            <Hamburger rounded />
+        <Col className="main-header">
+            <Hamburger rounded toggled={isOpen} toggle={setOpen} />
+            <div className="menu-nav">
+            <ul>
+              <li>Home</li>
+              <li>Portfolio</li>
+            </ul>
+          </div>
         </Col>
       </Row>
     </Container>

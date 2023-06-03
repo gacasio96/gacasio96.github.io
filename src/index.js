5b1b113503bc549d6b9e3e6b0dccd2e8182bea43
gacasio96 <geordetteacasio2018@gmail.com>
2023-06-03 20:11:31 +0800

    install reactjs router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import Header from './Header';
 import App from './App';
@@ -9,11 +10,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Header />
-    <App />
-    <Footer />
-  </React.StrictMode>
+    <React.StrictMode>
+      <Header />
+      <App />
+      <Footer />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
